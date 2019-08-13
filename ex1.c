e3a34776b45fbc8ae8ee15bb5cf491a7fcc5b333
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-08-13 15:31:08 +0800

    added structs

--- ex1.c
+++ ex1.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+
+typedef struct {
+    char* data;
+} word;
+
+typedef struct {
+    word* data;
+    int word_count;//the number of words in a sentence
+} sentence;
+
+typedef struct {
+    sentence* data  ;
+    int sentence_count;//the number of sentences in a paragraph
+} paragraph;
+
+typedef struct {
+    paragraph* data;
+    int paragraph_count;//the number of paragraphs in document
+} document
+
 int main(){
 
   return 0;
