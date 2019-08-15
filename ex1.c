abcdbd6734bd29517db4dd2453bf9f0ac3fa9f00
Justin Po <justin.andre.po@gmail.com>
2019-08-15 11:05:12 +0800

    append word works now

--- ex1.c
+++ ex1.c
@@ -23,6 +23,7 @@ typedef struct {
 
 int initWord(word*);
 int initSentence(sentence*);
+int printWord(word);
 int getWords(sentence*, char*);
 int appendWord(sentence*, word*);
 void addWordCount(sentence*);
@@ -30,7 +31,7 @@ void addWordCount(sentence*);
 int main(){
 
     char s[10000] = "hello there people";
-    sentence *newSentence;
+    sentence *newSentence = malloc(sizeof(char)*strlen(s));
 
     initSentence(newSentence);
     getWords(newSentence, s);
@@ -39,24 +40,26 @@ int main(){
 }
 
 int initWord(word *w) {
-    printf("here");
     w->data = NULL; 
-    printf("here 2");
-
     return 1;
 }
 
 int initSentence(sentence *s) {
     s->word_count = 0;
-    initWord(s->data);
+    s->data = NULL;
+    return 1;
+}
 
+int printWord(word w) {
+    printf("%s\n", w.data);
     return 1;
 }
 
 int appendWord(sentence *s, word *w) {
     word *tmp = malloc(sizeof(char) * strlen(w->data));
+    int i;
 
-    for(int i=0; i < s->word_count; i++)
+    for(i = 0; i < s->word_count; i++)
         tmp[i] = s->data[i];
 
     free(s->data);
@@ -66,9 +69,8 @@ int appendWord(sentence *s, word *w) {
     if(s->data==NULL)
         return 0;
     else {
-        for(int i = 0; i<s->word_count; i++)
+        for(i = 0; i<s->word_count; i++)
             s->data[i] = tmp[i];
-        // strcpy(s->data[s->word_count++], w);
         s->data[s->word_count++] = *w;
 
         free(tmp);
@@ -80,22 +82,22 @@ int appendWord(sentence *s, word *w) {
 }
 
 int getWords(sentence *s, char *arr) {
-    word *newWord;
+    word *newWord = malloc(sizeof(char) * strlen(arr));
     char *tok;
-
-    tok = strtok(arr, " ");
 
     initWord(newWord);
 
+    tok = strtok(arr, " ");
+
     while(tok != NULL) {
-        printf("%s\n", tok);
-        printf("yes");
         newWord->data = tok;
-        // strcpy(newWord->data, tok);
-        // appendWord(s, newWord);
+        appendWord(s, newWord);
         tok = strtok(NULL, " ");
     }
 
-    
+    for(int i = 0; i < s->word_count; i++) {
+        printWord(s->data[i]);
+    }
+
     return 1;
 }
