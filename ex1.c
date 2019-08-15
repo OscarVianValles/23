a7fe5520d1b141ee00540ae2b51e1a3d6d741bb3
Justin Po <justin.andre.po@gmail.com>
2019-08-15 11:26:09 +0800

    added comments

--- ex1.c
+++ ex1.c
@@ -31,35 +31,40 @@ void addWordCount(sentence*);
 int main(){
 
     // Sentence to Words Sample
-    // char s[10000] = "hello there people";
-    // sentence *newSentence = malloc(sizeof(char)*strlen(s));
+    char s[10000] = "hello there people";
+    sentence *newSentence = malloc(sizeof(char)*strlen(s));
 
-    // initSentence(newSentence);
-    // getWords(newSentence, s);
+    initSentence(newSentence);
+    getWords(newSentence, s);
 
     return 0;
 }
 
 int initWord(word *w) {
+    // initialize word data
     w->data = NULL; 
     return 1;
 }
 
 int initSentence(sentence *s) {
+    // initialize sentence data
     s->word_count = 0;
     s->data = NULL;
     return 1;
 }
 
 int printWord(word w) {
+    // prints word
     printf("%s\n", w.data);
     return 1;
 }
 
 int appendWord(sentence *s, word *w) {
+    // allocating memory for temporary word
     word *tmp = malloc(sizeof(char) * strlen(w->data));
     int i;
 
+    // ???
     for(i = 0; i < s->word_count; i++)
         tmp[i] = s->data[i];
 
@@ -83,22 +88,27 @@ int appendWord(sentence *s, word *w) {
 }
 
 int getWords(sentence *s, char *arr) {
+    // function to extract words out of a sentence
     word *newWord = malloc(sizeof(char) * strlen(arr));
     char *tok;
 
+    // initialize word value
     initWord(newWord);
 
+    // tokenizing process start
     tok = strtok(arr, " ");
 
     while(tok != NULL) {
+        // assign tokenized string to word data
         newWord->data = tok;
+        // append word to sentence
         appendWord(s, newWord);
         tok = strtok(NULL, " ");
     }
+    // tokenizing process end
 
-    for(int i = 0; i < s->word_count; i++) {
+    for(int i = 0; i < s->word_count; i++)
         printWord(s->data[i]);
-    }
 
     return 1;
 }
