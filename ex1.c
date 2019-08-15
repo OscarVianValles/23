cc5394acf6bdcbe781188449374ea42d1b354bee
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-08-15 12:58:28 +0800

    added working read-file

--- ex1.c
+++ ex1.c
@@ -13,45 +13,60 @@ typedef struct {
 
 typedef struct {
     word* data;
-    int word_count;//the number of words in a sentence
+    int word_count;
 } sentence;
 
 typedef struct {
-    sentence* data  ;
-    int sentence_count;//the number of sentences in a paragraph
+    sentence* data;
+    int sentence_count;
 } paragraph;
 
 typedef struct {
     paragraph* data;
-    int paragraph_count;//the number of paragraphs in document
+    int paragraph_count;
 } document;
 
 typedef struct {
-  char* data;
+    char** data;
+    int query_count;
 } query;
 
-void readFile(char* fileName, char* buffer, paragraph* docParagraphs);
-// void initializeQueries(query *docQuery);
+void readFile(char* fileName, char text[][CHARACTER_LIMIT], document* myDocument, query* myQuery);
+void initializeDocument(char text[][CHARACTER_LIMIT], document* myDocument);
 
 int main(int argc, char *argv[]){
 
   //Initialize Document
-  query docQuery[QUERY_LIMIT];
-  document myDoc;
-  paragraph docParagraphs[PARAGRAPH_LIMIT];
-  char buffer[2000] = {0};
-  readFile(argv[1], buffer, docParagraphs);
 
+  //Intializing Text
+  char text[PARAGRAPH_LIMIT][CHARACTER_LIMIT];
+
+  //Initializing main document variables
+  query *myQuery = malloc(sizeof(query));
+  document *myDocument = malloc(sizeof(document));
+
+  //Reading from file
+  readFile(argv[1], text, myDocument, myQuery);
+
+  for(int i = 0; i < myQuery->query_count; i++){
+    printf("%s", myQuery->data[i]);
+  };
+
+  for(int i = 0; i < myQuery->query_count; i++){
+    free(myQuery->data[i]);
+  }
+
+  free(myQuery);
+  free(myDocument);
+  //Intializing document from file
+  void initializeDocument(text, myDocument);
   return 0;
 }
 
-/* void initializeQueries(query *docQuery){
-  for(int i = 0; i < QUERY_LIMIT; i++){
-  }
-}
-*/
-
-void readFile(char* fileName, char* buffer, paragraph* docParagraphs){
+void readFile(char* fileName, char text[][CHARACTER_LIMIT], document* myDocument, query* myQuery){
+  //Initializing variables
+  char buffer[2000] = {0};
+  char** queries;
 
   //Opening File
   FILE *fp;
@@ -63,15 +78,42 @@ void readFile(char* fileName, char* buffer, paragraph* docParagraphs){
     return;
   }
 
-  int counter = -1, paragraphCounter = 0;
+  //Parsing through the file
+  int paragraphCount = -1, queryCount = -1, counter = 0;
   while (fgets(buffer, BUFFER_LIMIT, fp) != NULL) {
-    if(counter == -1){
-      counter = atoi(buffer);
-    } else if (counter > 0) {
-      docParagraphs[paragraphCounter++] =
 
+    //Gets the number of paragraphs on the first loop
+    if(paragraphCount == -1){
+      paragraphCount = atoi(buffer);
+      myDocument->paragraph_count = paragraphCount;
+    }
+
+    //Gets the paragraph strings
+    else if (paragraphCount > 0) {
+      strcpy(text[counter++], buffer);
+      paragraphCount--;
+    }
+
+    //If paragraph count is 0, this means that the following line is now the queries
+    else if (paragraphCount == 0 && queryCount == -1){
+      queryCount = atoi(buffer);
+      myQuery->query_count = queryCount;
+      counter = 0;
+      queries = malloc(queryCount * sizeof(char*));
+      for (int i = 0; i < queryCount; i++){
+        queries[i] = malloc((CHARACTER_LIMIT) * sizeof(char));
+      }
+    }
+
+    //Save the queries to a temporary holder
+    else if (queryCount > 0){
+      strcpy(queries[counter++], buffer);
+      queryCount--;
     }
   }
+
+  //Save the queries to the struct
+  myQuery->data = queries;
 
   //Closes file
   fclose(fp);
