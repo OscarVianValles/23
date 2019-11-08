f8315267921f92043d1aa1cfb0b16176e33d42c9
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-11-08 15:37:53 +0800

    finished ex9

--- ex9/mgaSecure9.py
+++ ex9/mgaSecure9.py
@@ -12,7 +12,17 @@ class Monster(ABC):
         pass
 
 
-class NormalBokoblin:
+class Bokoblin(Monster):
+    @abstractmethod
+    def bludgeon(self):
+        pass
+
+    @abstractmethod
+    def defend(self):
+        pass
+
+
+class NormalBokoblin(Bokoblin):
     def bludgeon(self):
         print("Bokoblin bludgeons you with a boko club for 1 damage")
 
@@ -29,7 +39,51 @@ class NormalBokoblin:
             self.defend()
 
 
-class NormalMoblin:
+class BlueBokoblin(Bokoblin):
+    def bludgeon(self):
+        print("Blue bokoblin bludgeons you with a spiked boko club for 2 damage")
+
+    def defend(self):
+        print("Blue bokoblin defends itself with a spiked boko shield")
+
+    def announce(self):
+        print("A blue bokoblin appeared")
+
+    def move(self):
+        if randint(1, 3) > 1:
+            self.bludgeon()
+        else:
+            self.defend()
+
+
+class SilverBokoblin(Bokoblin):
+    def bludgeon(self):
+        print("Silver bokoblin bludgeons you with a dragonbone boko club for 5 damage")
+
+    def defend(self):
+        print("Silver bokoblin defends itself with a dragonbone boko shield")
+
+    def announce(self):
+        print("A silver bokoblin appeared")
+
+    def move(self):
+        if randint(1, 3) > 1:
+            self.bludgeon()
+        else:
+            self.defend()
+
+
+class Moblin(Monster):
+    @abstractmethod
+    def stab(self):
+        pass
+
+    @abstractmethod
+    def kick(self):
+        pass
+
+
+class NormalMoblin(Moblin):
     def stab(self):
         print("Moblin stabs you with a spear for 3 damage")
 
@@ -46,7 +100,51 @@ class NormalMoblin:
             self.kick()
 
 
-class NormalLizalflos:
+class BlueMoblin(Moblin):
+    def stab(self):
+        print("Blue moblin stabs you with a spear for 3 damage")
+
+    def kick(self):
+        print("Blue moblin kicks you for 1 damage")
+
+    def announce(self):
+        print("A blue moblin appeared")
+
+    def move(self):
+        if randint(1, 3) > 1:
+            self.stab()
+        else:
+            self.kick()
+
+
+class SilverMoblin(Moblin):
+    def stab(self):
+        print("Silver moblin stabs you with a spear for 3 damage")
+
+    def kick(self):
+        print("Silver moblin kicks you for 1 damage")
+
+    def announce(self):
+        print("A silver moblin appeared")
+
+    def move(self):
+        if randint(1, 3) > 1:
+            self.stab()
+        else:
+            self.kick()
+
+
+class Lizalflos(Monster):
+    @abstractmethod
+    def throwBoomerang(self):
+        pass
+
+    @abstractmethod
+    def hide(self):
+        pass
+
+
+class NormalLizalflos(Lizalflos):
     def throwBoomerang(self):
         print("Lizalflos throws its lizal boomerang at you for 2 damage")
 
@@ -63,17 +161,98 @@ class NormalLizalflos:
             self.hide()
 
 
+class BlueLizalflos(Lizalflos):
+    def throwBoomerang(self):
+        print("Blue lizalflos throws its lizal boomerang at you for 2 damage")
+
+    def hide(self):
+        print("Blue lizalflos camouflages itself")
+
+    def announce(self):
+        print("A blue lizalflos appeared")
+
+    def move(self):
+        if randint(1, 3) > 1:
+            self.throwBoomerang()
+        else:
+            self.hide()
+
+
+class SilverLizalflos(Lizalflos):
+    def throwBoomerang(self):
+        print("Silver lizalflos throws its lizal boomerang at you for 2 damage")
+
+    def hide(self):
+        print("Silver lizalflos camouflages itself")
+
+    def announce(self):
+        print("A silver lizalflos appeared")
+
+    def move(self):
+        if randint(1, 3) > 1:
+            self.throwBoomerang()
+        else:
+            self.hide()
+
+
+class Dungeon(ABC):
+    @abstractmethod
+    def newBokoblin(self) -> Bokoblin:
+        pass
+
+    @abstractmethod
+    def newMoblin(self) -> Moblin:
+        pass
+
+    @abstractmethod
+    def newLizalflos(self) -> Lizalflos:
+        pass
+
+
+class EasyDungeon(Dungeon):
+    def newBokoblin(self) -> Bokoblin:
+        return NormalBokoblin()
+
+    def newMoblin(self) -> Moblin:
+        return NormalMoblin()
+
+    def newLizalflos(self) -> Lizalflos:
+        return NormalLizalflos()
+
+
+class MediumDungeon(Dungeon):
+    def newBokoblin(self) -> Bokoblin:
+        return BlueBokoblin()
+
+    def newMoblin(self) -> Moblin:
+        return BlueMoblin()
+
+    def newLizalflos(self) -> Lizalflos:
+        return BlueLizalflos()
+
+
+class HardDungeon(Dungeon):
+    def newBokoblin(self) -> Bokoblin:
+        return SilverBokoblin()
+
+    def newMoblin(self) -> Moblin:
+        return SilverMoblin()
+
+    def newLizalflos(self) -> Lizalflos:
+        return SilverLizalflos()
+
+
 class Encounter:
-    def __init__(self):
+    def __init__(self, dungeon: Dungeon):
         self.__enemies = []
         for i in range(randint(0, 8)):
             r = randint(1, 3)
             if r == 1:
-                self.__enemies.append(NormalBokoblin())
+                self.__enemies.append(dungeon.newBokoblin())
             elif r == 2:
-                self.__enemies.append(NormalMoblin())
+                self.__enemies.append(dungeon.newMoblin())
             else:
-                self.__enemies.append(NormalLizalflos())
+                self.__enemies.append(dungeon.newLizalflos())
 
     def announceEnemies(self):
         print("%d monsters appeared" % len(self.__enemies))
@@ -86,6 +265,8 @@ class Encounter:
 
 
 if __name__ == "__main__":
-    encounter = Encounter()
+    # encounter = Encounter(EasyDungeon())
+    # encounter = Encounter(MediumDungeon())
+    encounter = Encounter(HardDungeon())
     encounter.announceEnemies()
     encounter.moveEnemies()
